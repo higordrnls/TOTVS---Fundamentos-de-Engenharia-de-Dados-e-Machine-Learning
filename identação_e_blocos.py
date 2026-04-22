2a8bc59a29f297889de2f490eabe4b3d9fccdf92
Higor Dornelas <higor.dornelas@edu.uniube.br>
2026-04-22 23:40:51 +0000

    Fix formatting in sacar and depositar functions for consistency

--- identação_e_blocos.py
+++ identação_e_blocos.py
@@ -3,14 +3,15 @@ def sacar(valor):
     
     if saldo >= valor:
         print("Valor sacado!")
-        print("Retire o seu dinheiro na boca do caixa.")
+        print("Retire o seu dinheiro na boca do caixa. ")
         
     print("Obrigado por ser nosso cliente, tenha um bom dia!")
 
 
-def depositar (valor):  
-    saldo = 500 
-    saldo += valor   
-     
-       
+def depositar(valor):
+    saldo = 500
+    saldo+= valor
+
+
 sacar(1000)
+        
